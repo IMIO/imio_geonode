6590f41c79b019104aa47ed9e2b1979674d0374a
TeamGeode <team@opengeode.be>
2015-02-27 09:26:46 +0000

    ajout action updatelayer dans toutes les sous catégorie (voir mail)

--- adminimio/admin.py
+++ adminimio/admin.py
@@ -7,4 +7,8 @@ from adminimio.models import Im
 class ImAdmin(admin.ModelAdmin):
     pass
 
+def update_layer_from_geoserver(modeladmin, request, queryset):
+    call_command('updatelayers')
+
+admin.site.add_action(update_layer_from_geoserver, 'Update layer from Geoserver')
 admin.site.register(Im,ImAdmin)
