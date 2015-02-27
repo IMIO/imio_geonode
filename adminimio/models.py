0433b991c72935571ce063de215fd12083d79fda
TeamGeode <team@opengeode.be>
2015-02-27 12:50:30 +0000

    Ajout d une option dans le menu profil (ajouter a geonode le block dans base.html)

--- adminimio/models.py
+++ adminimio/models.py
@@ -9,12 +9,14 @@ class Im(models.Model):
 
     @staticmethod
     def updatelayer():
+        print('   !!! dans updatelayer() de adminimio !!!')
         call_command('updatelayers')
 
     def my_task_init(self):
         return mark_safe("<img class='loading' src='/static/img/loading.gif' alt='loading' style='display:none;' /><a data-identifier='task_%i' class='task'><img src='/static/img/process.png' style='cursor:pointer;' /></a>") % self.id
     my_task_init.allow_tags = True
     my_task_init.short_description = _(u"Execute Task")
+
 
 def profile_post_save(instance, sender, **kwargs):
     """En vue de recupere des infos lors de l ajout d un utilisateur"""
