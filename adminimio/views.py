acde187f66e9db4721c00fe840f9712e850d5c9b
TeamGeode <team@opengeode.be>
2015-03-02 10:49:04 +0000

    security add on the adminimmio page

--- adminimio/views.py
+++ adminimio/views.py
@@ -5,10 +5,52 @@ import logging
 import shutil
 import traceback
 
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.conf import settings
+from django.template import RequestContext, loader
+from django.utils.translation import ugettext as _
+from django.utils import simplejson as json
+from django.utils.html import escape
+from django.template.defaultfilters import slugify
+from django.forms.models import inlineformset_factory
+from django.db.models import F
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
+from functools import wraps
 
 from adminimio.models import Im
 
-#def index(request):
-#    posts = Post.objects.filter(published=True)
-#    return render(request,'blog/index.html',{'posts':posts})
+
+def is_auth(view_func):
+    """
+    Decorator for views that checks that the user is logged in and is a staff
+    member, displaying the login page if necessary.
+    """
+    def _checklogin(request, *args, **kwargs):
+        if request.user.is_active and request.user.is_staff:
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponse(
+            loader.render_to_string(
+                '401.html', RequestContext(
+                    request, {
+                        'error_message': _("You are not allowed to view this page.")})), status=403)
+
+    return wraps(view_func)(_checklogin)
+
+
+@is_auth
+def admin_view_imio(request, template='adminimio/imio_management.html'):
+    try:
+        context_dict = {}
+        return render_to_response(template, RequestContext(request, context_dict))
+    except PermissionDenied: # maybe need to be remove
+        return HttpResponse(
+            loader.render_to_string(
+                '401.html', RequestContext(
+                    request, {
+                        'error_message': _("You are not allowed to view this page.")})), status=403)
