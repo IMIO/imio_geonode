6685e06cfeee0c9b2c0a2acdeb4d98690d9ca553
Benoît Suttor <benoit.suttor@imio.be>
2015-07-03 10:13:03 +0200

    Use geonode-imports database name as in geonode doc

--- Dockerfiles/postgis/initdb-postgis.sh
+++ Dockerfiles/postgis/initdb-postgis.sh
@@ -6,10 +6,12 @@ CREATE ROLE geonode ENCRYPTED PASSWORD 'geonode' LOGIN;
 EOSQL
 $POSTGRES postgres --single -E <<EOSQL
 CREATE DATABASE geonode OWNER geonode ;
-CREATE DATABASE geonode_data OWNER geonode ;
+CREATE DATABASE "geonode-imports" OWNER geonode ;
 EOSQL
 $POSTGRES pg_ctl -w start
-$POSTGRES psql -h localhost -p 5432 -d geonode_data -c 'CREATE EXTENSION postgis;'
-$POSTGRES psql -h localhost -p 5432 -d geonode_data -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
-$POSTGRES psql -h localhost -p 5432 -d geonode_data -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+$POSTGRES psql -d geonode-imports -c 'CREATE EXTENSION postgis;'
+$POSTGRES psql -d geonode-imports -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+$POSTGRES psql -d geonode-imports -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+# $POSTGRES psql -d geonode-imports -f '/docker-entrypoint-initdb.d/geonode.dump'
+# $POSTGRES psql -d geonode-imports -f '/docker-entrypoint-initdb.d/geonode-imports.dump'
 $POSTGRES pg_ctl stop
