ee2cbcecdfedbb1680b72e158edd64331950a0e6
Benoît Suttor <benoit.suttor@imio.be>
2015-11-23 14:32:37 +0100

    Add classifiers and changes

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ gdal_version = '1.10.1'
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('CHANGES.rst')
+    + '\n')
+
+
 install_requires = [
     'geonode',
     # 'geonode'.format(genode_version),
@@ -21,14 +28,17 @@ setup(
     author="",
     author_email="",
     description="imio_geonode, based on GeoNode",
-    long_description=(read('README.rst')),
+    long_description=long_description,
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 1 - Planning',
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Framework :: Django",
     ],
     license="BSD",
-    keywords="imio_geonode geonode django",
+    keywords="imio_geonode geonode django imio",
     url='https://github.com/imio/imio_geonode',
     packages=['imio_geonode',],
     include_package_data=True,
