f802e0cc8e4d2d55c643e3878978ebe3c45d9a08
ndufrane <dufrane.nicolas@gmail.com>
2017-12-11 13:32:58 +0100

    Back to development: 0.2.39

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name="imio_geonode",
-    version='0.2.38',
+    version='0.2.39.dev0',
     author="",
     author_email="",
     description="imio_geonode, based on GeoNode",
